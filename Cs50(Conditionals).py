1a184d148a4e82c6ce51c36ad6d2224df731b967
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-12-09 21:14:31 +0100

    09/12/23

--- Cs50(Conditionals).py
+++ Cs50(Conditionals).py
@@ -1,18 +1,35 @@
-#EVEN/ODD? FUNCTION
-def main():
-    x = int(input("Enter a number: "))
-    if is_even(x):
-        print("As Even as the river flows!")
-    else:
-        print("As Odd as snow in spring!")
+# #EVEN/ODD? FUNCTION
+# def main():
+#     x = int(input("Enter a number: "))
+#     if is_even(x):
+#         print("As Even as the river flows!")
+#     else:
+#         print("As Odd as snow in spring!")
 
-def is_even(number):
-    # even_or_odd = number % 2 == 0
-    # return even_or_odd
-    if number % 2 == 0:
-        return True
-    else:
-        return False
-    #return number % 2 == 0
-main()
-    
+# def is_even(number):
+#     # even_or_odd = number % 2 == 0
+#     # return even_or_odd
+#     if number % 2 == 0:
+#         return True
+#     else:
+#         return False
+#     #return number % 2 == 0
+# main()
+
+friends_name = input("Enter Friends Character: ")
+
+match friends_name:
+    case "Chandler" | "Chandler Bing":
+        print("Sarcastic, Funny, Caring")
+    case "Monica" | "Monica Geller":
+        print("High Maintenance, Loving, Quirky")
+    case "Joey" | "Joey Tribianni":
+        print("Foodie, Dashing, Ladies Magnet")
+    case "Ross" | "Ross Geller":
+            print("Insecure, Nice, Thoughtful") 
+    case "Rachel" | "Rachel Green":
+        print("Selfish, Independent, Beautiful")
+    case "Phoebe" | "Phoebe Buffay":
+        print("Not Funny, Anti Mondler, Singer")
+    case _:
+        print("Uhm, Who?")
