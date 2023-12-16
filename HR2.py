f500fe0cc0e0cc01511585d92c7ce6a9d2a6e99f
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-12-16 16:39:05 +0100

    16/12/23

---
+++ HR2.py
@@ -0,0 +1,64 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+# if __name__ == '__main__':
+#     n = int(input().strip())
+# if n % 2 == 0 and n >=2 and n < 6:
+#     print("Not weird")
+# elif n % 2 == 0 and n >= 6 and n < 21:
+#     print("Weird")
+# elif n % 2 == 0 and n > 20:
+#     print("Not Weird")
+# else:
+#     print("Weird")  
+
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+#     sum = a + b
+#     diff = a - b
+#     product = a * b
+    
+#     print(sum)
+#     print(diff)
+#     print(product)
+
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+    
+#     i_div = a//b
+#     f_div = a/b
+    
+#     print(i_div)
+#     print(f_div)
+
+# n = int(input())
+# for i in range(n):
+#     print(pow(i, 2))
+    
+    
+# def is_leap(year):
+#     if year % 4 == 0 and year % 100 != 0:
+#         return True
+#     elif year % 100 == 0 and year % 400 == 0:
+#         return True
+#     else:
+#         return False
+         
+    
+
+
+# year = int(input())
+# print(is_leap(year))
+
+
+n = int(input())
+for number in range(1,n+1):
+    print(number, end='')
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,15 +1,1 @@
-from SquareFunction import main  # Importing the main function from your module
 
-def test_user_input():
-    print("Testing number input:")
-    user_input = "8"  # Simulate number input '8'
-    result = main(user_input)  # Pass user input to the main function
-
-    if result == "64":
-        print("Number input test passed")
-    else:
-        print("Number input test failed")
-
-
-if __name__ == '__main__':
-    test_user_input()
