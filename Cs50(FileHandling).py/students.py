bd34495d68cae044cc7b6b9b584c175cc0eefa14
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-12-17 18:23:16 +0100

    Update 17/12/23

--- Cs50(FileHandling).py/students.py
+++ Cs50(FileHandling).py/students.py
@@ -2,12 +2,14 @@
 # with open("Cs50(FileHandling).py/students.txt", "a") as file:
 #     file.write(f"{student}\n")
 # # file.close()
+
+
 from time import sleep
-with open("Cs50(FileHandling).py/students.txt", "r") as file:
-    print(f"Students in SS2 are: ")
-    for line in sorted(file):
-        sleep(2)
-        print(line, end = "")
+# with open("Cs50(FileHandling).py/students.txt", "r") as file:
+#     print(f"Students in SS2 are: ")
+#     for line in sorted(file):
+#         sleep(2)
+#         print(line, end = "")
 
 
 #Sorted list of students
@@ -29,3 +31,19 @@ with open("Cs50(FileHandling).py/students.txt", "r") as file:
 #     sleep(2)
 #     #print the students list but sorted
 #     print(student, end = "")
+
+
+
+# There is only space for 7 students in SS2. Exit the program if an attempt is made to input an eighth student
+student = input("Enter SS2 student name: ")
+with open("Cs50(FileHandling).py/students.txt", "r") as file:
+    #readlines method reads lines from the file and returns a list of students. Len returns the number of students in the list.
+    if len(file.readlines()) > 7:
+        print ("No more students allowed in SS2.")
+        sleep(1)
+        with open("Cs50(FileHandling).py/rejected_students.txt", "a") as file2:
+             file2.write(f"{student}\n")
+        SystemExit()
+    else:
+        with open("Cs50(FileHandling).py/students.txt", "a") as file3:
+            file3.write(f"{student}\n")
