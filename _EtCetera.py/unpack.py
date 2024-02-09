544193aa2d93494b727148bb29ed2ac277cc82f9
hamzakhan663 <sulaimonhamza233@gmail.com>
2024-02-09 16:59:31 +0100

    9/2/24

--- _EtCetera.py/unpack.py
+++ _EtCetera.py/unpack.py
@@ -1,2 +1,14 @@
+#Unpacking
+
+from time import sleep
 last, first, middle = input("What's your name?: ").split(" ")
 print(f"Hi, {first}.")
+
+sleep(1)
+
+def total(galleons, sickles, knuts):
+    #calculate total number of money in sickles
+    return round(((galleons * 17 ) + sickles) + (knuts/29),2)
+
+coins = [50, 100, 2000]
+print(total(*coins), 'Sickles')
