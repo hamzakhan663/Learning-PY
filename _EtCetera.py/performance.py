e931c1a68d40eaabead2c675f6a09cc6e27642ed
hamzakhan663 <sulaimonhamza233@gmail.com>
2024-02-09 13:01:51 +0100

    9/2/24

--- _EtCetera.py/performance.py
+++ _EtCetera.py/performance.py
@@ -1,4 +1,8 @@
+import sys
 
+sys.path.append('C:/Users/HAMZAT/Desktop/Python Learning/_UnitTests')
+
+import Add_Subfunction
 def overall(obj, practical, project):
     return (
         ((80/100)*obj) + ((10/100)*practical) + ((10/100)*project)
