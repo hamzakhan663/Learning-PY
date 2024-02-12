46f3ae442428f58056ddcd9c0d3e380e81cdd3e9
hamzakhan663 <sulaimonhamza233@gmail.com>
2024-02-12 22:44:08 +0100

    12/2/24

---
+++ _EtCetera.py/dist_comp.py
@@ -0,0 +1,15 @@
+#Dictionary Comprehension
+
+friends = ["Chandler", "Monica", "Rachel"]
+
+houses = {friend:"Central Perk" for friend in friends}
+
+print (houses)
+
+
+# houses = []
+# for friend in friends:
+    
+#     houses.append({"name": friend, "house" : "Central Perk"})
+
+# print (houses)
---
+++ _EtCetera.py/enum.py
@@ -0,0 +1,6 @@
+friends = ["Ross", "Phoebe", "Joey","Chandler", "Monica", "Rachel"]
+
+#Indexed list using enumerate
+for i, friend in enumerate(friends):
+    print(i + 1, friend)
+    
